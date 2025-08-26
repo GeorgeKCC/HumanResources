b601d81d2f3e6b214c898f5da4e0bdbc661e87cc
GeorgeCalderonCarranza <george.calderon@flyr.com>
2025-08-26 16:48:12 -0500

    Add controller Colaborator

---
+++ Api/Controllers/ColaboratorController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HumanResourcesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColaboratorController : ControllerBase
+    {
+    }
+}
